9d684933c6b888f7c7569d1b530e1f848afe5fb1
Vasilis Souvatzis <vasouv@gmail.com>
2023-03-05 02:08:59 +0200

    Initial commit

---
+++ temperature-service-alpha/src/main/java/vs/temperatureservicealpha/TemperatureServiceAlphaApplication.java
@@ -0,0 +1,13 @@
+package vs.temperatureservicealpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureServiceAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureServiceAlphaApplication.class, args);
+	}
+
+}
---
+++ temperature-service-alpha/src/test/java/vs/temperatureservicealpha/TemperatureServiceAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.temperatureservicealpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureServiceAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
